e94b25de9f70fbb119227292c41e2c9bef93531a
leonel <leoonelvj@gmail.com>
2025-12-17 02:56:55 -0300

    Añadir axios, enrutamiento y diseño base

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import MainLayout from '../components/layout/MainLayout';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
+    <MainLayout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+    </MainLayout>
     </BrowserRouter>
   );
 }
